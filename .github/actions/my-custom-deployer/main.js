8e0bc412dff6466eb3bda3ce8dbded6ec244921c
Andrew Penn <A.C.Penn@sussex.ac.uk>
2026-07-07 09:56:10 +0100

    Create main.js

---
+++ .github/actions/my-custom-deployer/main.js
@@ -0,0 +1,3 @@
+console.log("🚀 Custom Node Deployer script triggered successfully!");
+console.log("Current working directory: ", process.cwd());
+// Add any minor file validation logic here if needed
